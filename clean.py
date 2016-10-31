a6a6d1247447f2a95ec187dcd16d21eb3bf674a2
Tianhua Fang <tf23772columbia.edu>
2016-10-31 17:57:47 -0400

    erase binary file

--- clean.py
+++ clean.py
@@ -1,3 +1,5 @@
 import os
 
-os.system('rm parser_test.ml parser_test.mli ')
+os.system('rm parser_test.ml parser_test.mli parser_test.cmi parser_test.cmo')
+os.system('rm ast_test.cmi')
+os.system('rm scanner.ml scanner.cm*')
